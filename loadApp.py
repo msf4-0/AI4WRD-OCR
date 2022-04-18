2403c144e2f5ee8a2e2ea0f48fa3fad63f310cc6
nyeoWM <wmnicholasyeo@gmail.com>
2022-04-18 13:08:46 +0800

    feature: added the ability to iterate through available ports, identify active cameras and preview video before taking a screenshot

--- loadApp.py
+++ loadApp.py
@@ -21,17 +21,53 @@ def clearsessState():
         del st.session_state[key]
     mainApp()
 
+
+@st.cache
+def get_available_devices():
+    print("cache miss")
+    available_devices = []
+    inactive_ports = []
+    port_num = 0
+    while len(inactive_ports) < 5:
+        potential_cam = cv2.VideoCapture(port_num)
+        if potential_cam.isOpened() == False:
+            inactive_ports.append(port_num)
+        else:
+            # available_devices.append(port_num)
+            is_active, _ = potential_cam.read()
+            if is_active:
+                available_devices.append(port_num)
+            else:
+                inactive_ports.append(port_num)
+        port_num += 1
+        potential_cam.release()
+
+
+    return available_devices
+
+def get_frame(camera_choice):
+    # st.session_state.vid = cv2.VideoCapture(camera_choice, cv2.CAP_DSHOW)
+    _, frame = st.session_state.vid.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    st.session_state['cap'] = frame
+
+    # st.session_state['cap'] = ImageGrab.grab(bbox=(115, 143, 1069, 1083))
+    st.session_state['cap'] = Image.fromarray(frame)
+
+    st.caption("Frame loaded. Proceed to crop tool.")
+    FRAME_WINDOW3 = st.image([])
+
+    FRAME_WINDOW3.image(frame)
+
+
 def mainApp():
-    
+
     st.header("HDMI Capture")
-    
+
     options = st.multiselect(
-            'Select languages to read',
-            ['English', 'Chinese'])
-
-    
-
-    
+        'Select languages to read',
+        ['English', 'Chinese'])
 
     if 'lang' not in st.session_state:
         st.session_state['lang'] = ""
@@ -47,37 +83,55 @@ def mainApp():
 
     #st.write('You selected:', options)
         
-  
-    
-    run = st.checkbox('Run')
-    status = st.empty()
-    
-    #FRAME_WINDOW2 = st.image([])
-     
+    # print(options)
 
-    if run:
+    available_devices = get_available_devices()
+    camera_choice = st.selectbox( 'Select Video Capture Device',    available_devices)
+    st.write("You selected camera number", camera_choice)
 
-        if 'vid' not in st.session_state:
-            st.session_state['vid'] = []
 
-        if 'cap' not in st.session_state:
-            status.subheader("Frame Loading...")
-            st.session_state.vid = cv2.VideoCapture(0)
-            # st.write("Video Capture done")
-            # vid.set(3, 1920)
-            # vid.set(4, 1080)
+    if len(available_devices) != 0:
+        run = st.checkbox('Run')
+        status = st.empty()
 
+        FRAME_WINDOW2 = st.image([])
+
+        if run:
+            if 'vid' not in st.session_state:
+                st.session_state['vid'] = []
+
+
+            st.session_state.vid = cv2.VideoCapture(camera_choice, cv2.CAP_DSHOW)
             st.session_state.vid.set(3, 1280)
             st.session_state.vid.set(4, 720)
 
-            _, frame = st.session_state.vid.read()
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            
-            st.session_state['cap'] = frame
-            
-            # st.session_state['cap'] = ImageGrab.grab(bbox=(115, 143, 1069, 1083))
-            st.session_state['cap'] = Image.fromarray(frame)
-            status.subheader("Frame loaded. Proceed to crop tool.")
+            if 'cap' not in st.session_state:
+                pass
+                # status.subheader("Frame Loading...")
+
+                capture_screenshot = st.button("capture screenshot")
+                if capture_screenshot:
+                    # cv2.destroyAllWindows()
+                    get_frame(camera_choice)
+                    # status.subheader("Frame loaded. Proceed to crop tool.")
+
+
+
+            status.subheader("Video Preview")
+            while run:
+                # st.write("Video Capture done")
+                # vid.set(3, 1920)
+                # vid.set(4, 1080)
+
+                _, frame = st.session_state.vid.read()
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                FRAME_WINDOW2.image(frame)
+
+
+
+
+
+
 
             
 
