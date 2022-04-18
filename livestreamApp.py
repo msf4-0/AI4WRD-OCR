b0111f69142fd1dd956c4783e3e6bbf11406f599
nyeoWM <wmnicholasyeo@gmail.com>
2022-04-18 13:12:16 +0800

    feature: rough code for the ability to publish csv to mqtt

--- livestreamApp.py
+++ livestreamApp.py
@@ -1,3 +1,4 @@
+import random
 import streamlit as st
 import cv2
 import json
@@ -13,6 +14,24 @@ from PIL import Image, ImageEnhance
 import csv
 from datetime import datetime
 
+from paho.mqtt import client as mqtt_client
+import pandas as pd
+
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+    else:
+        print("Failed to connect, return code %d\n", rc)
+
+
+@st.cache
+def connect_mqtt(client_id, broker, port):
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
 
 def mainApp():
     if 'cropArr' not in st.session_state:
@@ -54,8 +73,43 @@ def mainApp():
         header = ['Timestamp','Crop ID', 'OCR Data']
 
         status = st.empty()
+
+        # todo dont use st.write
+        path_to_save = st.text_input('Path to save', '')
+        st.write('The current path is', path_to_save)
+
+
         saveallCSV = st.button("Save Previous to csv")
+
         savecontCSV = st.button("Save Continuous to csv")
+
+        """
+        Mqtt test 
+        """
+        status_mqtt = st.empty()
+        mqtt_address = st.text_input('Mqtt Broker Address', '')
+        mqtt_topic = st.text_input('Mqtt topic to publish to', '')
+        publish_mqtt = st.button("Publish to Mqtt server")
+
+        # mqtt test
+        broker = 'broker.emqx.io'
+        port = 1883
+        topic = "test123456"
+        client_id = f'python-mqtt-{random.randint(0, 1000)}'
+        # username = 'emqx'
+        # password = 'public
+        #
+        if publish_mqtt:
+            client = connect_mqtt(client_id, broker, port)
+
+
+        """
+        end of mqtt test
+        """
+
+
+        file_saving_status = st.empty()
+
 
         while counter < len(st.session_state.cropArr):
             st.session_state.d1["FRAME_WINDOW{0}".format(counter)] = st.image([])
@@ -114,34 +168,72 @@ def mainApp():
                     csvData = [datetime.now(), " Crop ID: %s" %(livecounter+1), st.session_state.text]
 
                     st.session_state.data.append(csvData)
-
-                    if saveallCSV:
-                        with open('ocrCSVData_all.csv', 'w', encoding='UTF8', newline='') as f:
-                            writer = csv.writer(f)
-
-                            # write the header
-                            writer.writerow(header)
-
-                            # write multiple rows
-                            writer.writerows(st.session_state.data)
-                            saveallCSV = False
-
                     if continuousSave == 1:
                         savecontCSV = 1
 
-                    if savecontCSV:
-                        with open('ocrCSVData_cont.csv', 'w', encoding='UTF8', newline='') as f:
-                            writer = csv.writer(f)
+                    if saveallCSV:
+                        if path_to_save != '':
+                            try:
+                                with open(path_to_save + ".csv", 'w', encoding='UTF8', newline='') as f:
+                                    writer = csv.writer(f)
 
-                            if continuousSave == 0:
-                                # write the header
-                                writer.writerow(header)
+                                    # write the header
+                                    writer.writerow(header)
 
-                            # write multiple rows
-                            writer.writerows(st.session_state.data)
-                        
-                        continuousSave = 1
+                                    # write multiple rows
+                                    writer.writerows(st.session_state.data)
+                                    saveallCSV = False
+                                file_saving_status.success("File Saved!")
 
+                            except Exception as e:
+                                print(e)
+                                file_saving_status.error("Error saving file")
+                        else:
+                            file_saving_status.error("Path not specified")
+
+
+
+
+
+                    elif savecontCSV:
+                        if path_to_save != '':
+                            try:
+                                with open('path_to_save', 'w', encoding='UTF8', newline='') as f:
+                                    writer = csv.writer(f)
+
+                                    if continuousSave == 0:
+                                        # write the header
+                                        writer.writerow(header)
+
+                                    # write multiple rows
+                                    writer.writerows(st.session_state.data)
+
+                                continuousSave = 1
+                                file_saving_status.info("Data stream is being appended to csv file")
+
+                            except Exception as e:
+                                print(e)
+                                file_saving_status.error("Error saving file")
+                        else:
+                            file_saving_status.error("Path not specified")
+
+                    elif publish_mqtt:
+                        msg_count = 0
+
+                        time.sleep(1)
+                        msg = f"messages: {msg_count}"
+
+                        df = pd.DataFrame(csvData)
+
+                        result = client.publish(topic, df.to_csv(index=False))
+
+                        # result: [0, 1]
+                        status = result[0]
+                        if status == 0:
+                            print(f"Send `{msg}` to topic `{topic}`")
+                        else:
+                            print(f"Failed to send message to topic {topic}")
+                        msg_count += 1
 
                     cv2.waitKey(0)
-                    livecounter+=1  
+                    livecounter+=1
